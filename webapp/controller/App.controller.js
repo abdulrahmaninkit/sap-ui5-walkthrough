1468e6b2a4c269c69b895070fee7083ec44d17b1
AbdulRahman <abdul.abdullah@inkitsolutions.com.au>
2025-04-04 01:41:52 +0530

    Lesson 6:Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
         onShowHello() {
 
-            alert("Hello World");
+            MessageToast.show("Hello World");
         }
     });
 });
