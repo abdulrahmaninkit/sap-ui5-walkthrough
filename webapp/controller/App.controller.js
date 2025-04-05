68ca4e99685a1d4867035f1a263139cd9874b185
AbdulRahman <abdul.abdullah@inkitsolutions.com.au>
2025-04-06 03:16:01 +0530

    Lesson 15:Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,16 +7,5 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("sap.ui5.walkthrough.controller.App", {
-        
-        onShowHello() {
-
-            // read msg from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            // show message
-            MessageToast.show(sMsg);
-        }
     });
 });
